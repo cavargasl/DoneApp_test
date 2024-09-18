88a0ba1fc6bfd107fc7f68c4816575d5d705cb5d
Camilo Vargas <camilovargasingm@gmail.com>
2024-09-18 13:36:46 -0500

    add functionality of select type of new task

--- App.tsx
+++ App.tsx
@@ -7,23 +7,44 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from "react-native-vector-icons/Entypo";
+import IconAwesome from "react-native-vector-icons/FontAwesome6";
 import TaskList from "./components/TaskList";
 import useTasks from "./hooks/useTask";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Badge from "./components/Badge";
+import { TypeTask } from "./types";
+import ListTypes from "./components/ListTypes";
 
 export default function App() {
   const { tasks, toggleTask, addTask, editTask } = useTasks();
   const inputRef = useRef<TextInput>(null);
+  const [openNewTask, setOpenNewTask] = useState(false);
+  const [typeSelected, setTypeSelected] = useState<TypeTask>("No list");
+  const [openedListType, setOpenedListType] = useState(false);
+  function onChangeType(type: TypeTask) {
+    setTypeSelected(type);
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>DoneApp</Text>
       <TaskList tasks={tasks} onToggle={toggleTask} onEditTask={editTask} />
-      <View style={styles.inputContainer}>
+      <View
+        style={[
+          styles.inputContainer,
+          openNewTask && styles.inputContainerFocus,
+        ]}
+      >
         <TextInput
           ref={inputRef}
-          style={styles.input}
+          style={[styles.input, openNewTask && styles.inputFocused]}
           placeholder="Write a new task"
           maxLength={40}
+          onPress={() => setOpenNewTask(true)}
+          onBlur={() => {
+            setOpenNewTask(false);
+            setOpenedListType(false);
+            setTypeSelected("No list");
+          }}
           onSubmitEditing={(event) => {
             const newTask = event.nativeEvent.text;
             if (newTask.trim().length > 0) {
@@ -31,16 +52,32 @@ export default function App() {
                 id: tasks.length + 1,
                 completed: false,
                 text: newTask,
-                type: "No list",
+                type: typeSelected,
               });
               inputRef.current?.clear();
             }
           }}
         />
-        <TouchableOpacity style={styles.listButton}>
-          <Icon name="list" size={24} color="#000" />
-        </TouchableOpacity>
+        {!openNewTask ? (
+          <TouchableOpacity style={styles.listButton}>
+            <Icon name="list" size={24} color="#000" />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            style={styles.buttonType}
+            onPress={() => setOpenedListType((e) => !e)}
+          >
+            <Badge type={typeSelected} />
+            <Text style={{ flex: 1 }}>{typeSelected}</Text>
+            <IconAwesome
+              name={openedListType ? "arrow-down" : "arrow-up"}
+              size={12}
+              color="#2b2b2b"
+            />
+          </TouchableOpacity>
+        )}
       </View>
+      {openedListType && <ListTypes onChangeType={onChangeType} />}
     </SafeAreaView>
   );
 }
@@ -60,25 +97,31 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     flexDirection: "row",
+    marginHorizontal: "auto",
     alignItems: "center",
+    width: "80%",
+  },
+  inputContainerFocus: {
+    width: "100%",
+    borderRadius: 10,
     backgroundColor: "#fff",
-    paddingVertical: 15,
-    paddingHorizontal: 20,
-    borderRadius: 50,
-    marginTop: 20,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
-    width: "90%",
-    alignSelf: "center",
+    height: "auto",
+    paddingHorizontal: 12,
   },
   input: {
     flex: 1,
     fontSize: 16,
     color: "#6e6e6e",
+    backgroundColor: "#fff",
+    height: "auto",
+    padding: 12,
+    borderRadius: 50,
+    shadowColor: "#707070",
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 1,
   },
+  inputFocused: { shadowOpacity: 0, elevation: 0, paddingHorizontal: 0 },
   listButton: {
     backgroundColor: "#fff",
     padding: 12,
@@ -86,10 +129,19 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     marginLeft: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
+    shadowColor: "#707070",
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    elevation: 3,
+    elevation: 1,
+  },
+  buttonType: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    backgroundColor: "#e2e2e2",
+    padding: 8,
+    borderRadius: 6,
+    width: 110,
+    justifyContent: "center",
   },
 });
---
+++ components/ListTypes.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { TypeTask } from "../types";
+import Badge from "./Badge";
+
+type ListTypeProps = {
+  onChangeType: (type: TypeTask) => void;
+};
+export default function ListTypes({ onChangeType }: ListTypeProps) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.buttons}
+        onPress={() => onChangeType("Personal")}
+      >
+        <Badge type={"Personal"} />
+        <Text style={styles.text}>Personal</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttons}
+        onPress={() => onChangeType("Work")}
+      >
+        <Badge type={"Work"} />
+        <Text style={styles.text}>Work</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttons}
+        onPress={() => onChangeType("Errands")}
+      >
+        <Badge type={"Errands"} />
+        <Text style={styles.text}>Errands</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.buttons}
+        onPress={() => onChangeType("No list")}
+      >
+        <Badge type={"No list"} />
+        <Text style={styles.text}>No list</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    marginTop: 10,
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20,
+    padding: 12,
+    paddingTop: 20,
+    gap: 20,
+  },
+  buttons: {
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+  },
+  text: {
+    fontSize: 20,
+  },
+});
